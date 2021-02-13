6ce6ca9a9a906c9b3397e6e632be5c71798da6d5
benfoxall <benfoxall@gmail.com>
2021-02-13 05:47:02 +0000

    Deploying to gh-pages from  @ deb91d60cf3af76108bb10d43003021d2c4f70a1 🚀

--- dist/App.js
+++ dist/App.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from "../_snowpack/pkg/react.js";
-import logo from "./logo.svg.proxy.js";
 import "./App.css.proxy.js";
 function App({}) {
   const [count, setCount] = useState(0);
@@ -9,17 +8,6 @@ function App({}) {
   }, [count, setCount]);
   return /* @__PURE__ */ React.createElement("div", {
     className: "App"
-  }, /* @__PURE__ */ React.createElement("header", {
-    className: "App-header"
-  }, /* @__PURE__ */ React.createElement("img", {
-    src: logo,
-    className: "App-logo",
-    alt: "logo"
-  }), /* @__PURE__ */ React.createElement("p", null, "Edit ", /* @__PURE__ */ React.createElement("code", null, "src/App.tsx"), " and save to reload."), /* @__PURE__ */ React.createElement("p", null, "Page has been open for ", /* @__PURE__ */ React.createElement("code", null, count), " seconds."), /* @__PURE__ */ React.createElement("p", null, /* @__PURE__ */ React.createElement("a", {
-    className: "App-link",
-    href: "https://reactjs.org",
-    target: "_blank",
-    rel: "noopener noreferrer"
-  }, "Learn React"))));
+  }, /* @__PURE__ */ React.createElement("header", null, /* @__PURE__ */ React.createElement("p", null, "Page has been open for ", /* @__PURE__ */ React.createElement("code", null, count), " seconds.")), /* @__PURE__ */ React.createElement("main", null), /* @__PURE__ */ React.createElement("footer", null));
 }
 export default App;
