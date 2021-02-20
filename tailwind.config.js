1680e1e5047a14f9140cb7604f27ef701ab90fc6
Ben Foxall <benfoxall@gmail.com>
2021-02-20 11:44:06 +0000

    tailwind optimise

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,8 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: ["./public/**/*.html", "./src/**/*.tsx"],
+  },
 }
